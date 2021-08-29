a30305139316a0ddbb175449784937ad7a371051
fangkyi02 <fangkyi02>
2021-08-29 23:02:39 +0800

    1.增加redux

--- remax/src/app.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  pages: ['pages/index/index','pages/test/index'],
-  window: {
-    navigationBarTitleText: 'Remax Wechat Template',
-    navigationBarBackgroundColor: '#282c34'
-  }
-};
